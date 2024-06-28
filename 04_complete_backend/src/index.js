27d043f7005097efa4d7ca263cc82097ad36b4cc
Abhishek <abhishekkori7902@gmail.com>
2024-06-28 22:13:09 +0530

    database connection

--- 04_complete_backend/src/index.js
+++ 04_complete_backend/src/index.js
@@ -1,8 +1,10 @@
+import express from "express"
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
 dotenv.config({
     path:'./env'
 })
+const app=express()
 
 connectDB()
 .then(()=>{
